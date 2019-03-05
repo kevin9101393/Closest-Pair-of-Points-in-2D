2522e47eba38f45d567db77388292984b05d98e7
王晟凱 <kevin9101393@gmail.com>
2019-03-06 00:36:31 +0800

    Update closestpointin2D.c
    will give correct result

--- closestpointin2D.c
+++ closestpointin2D.c
@@ -27,19 +27,26 @@ int main(void){
     // generate random 2D points
     srand(time(NULL));
     for(int i = 0 ; i<numRandomPoints ; i++){
-        points[i].x = rand() % 100 / 10.0;
-        points[i].y = rand() % 100 / 10.0;
-        printf("x = %.1f, y = %.1f\n", points[i].x, points[i].y);
+        points[i].x = rand() % 1000000 / 1000.0;
+        points[i].y = rand() % 1000000 / 1000.0;
+        // printf("x = %.1f, y = %.1f\n", points[i].x, points[i].y);
     }
 
+    clock_t begin = clock();
     //use O^2 method as answer
     findMinDistOSqr(points, numRandomPoints);
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("time spent for O^2 algorithm => %fs\n", time_spent);
 
-
+    begin = clock();
     // sort the array according to the x axis
     qsort(points, sizeof(points)/sizeof(point2D), sizeof(point2D), comparePointsX);
 
     double minDistance = findMinDist(points, numRandomPoints);
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("time spent for divide-and-conquer algorithm => %fs\n", time_spent);
 
     printf("Divide-and-Conquer result:\n");
     printf("The smallest distance is %.2f\n", minDistance);
@@ -82,28 +89,36 @@ double findMinDist(point2D *points, int len){
         for(int i = 0 ; i<len ; i++)
             median += points[i].x;
         median /= len;
-        
+
         // double d = 2;
-        int start, end;
+        int start = 0;
         for(int i = 0 ; i<len ; i++){
-            if(points[i].x >= median - (float)d){
+            if (points[i].x >= median - (float)d){
                 start = i;
                 break;
             }
         }
+        int end = start - 1;
         for (int i = start+1; i < len; i++){
             // printf("points[i].x = %.1f\n", points[i].x);
             // printf("median + (float)d = %.1f\n", median + (float)d); 
             if (points[i].x > median + (float)d){
+                // if(len == 5){
+                //     printf("points[%d].x = %.1f, diff = %.2f\n", i, points[i].x, median + (float)d);
+                // }
                 break;
             }
-            end = i;
+            else
+                end = i;
         }
-        // printf("len out = %d\n", len);
-        // printf("start = %d\n", start);
-        // printf("end = %d\n", end);
-        // printf("len = %d\n", end-start+1);
-        // printf("median = %.2f\n", median);
+        // if(len == 5){
+            // printf("len out = %d\n", len);
+            // printf("start = %d\n", start);
+            // printf("end = %d\n", end);
+            // printf("len = %d\n", end-start+1);
+            // printf("median = %.2f\n", median);
+            // printf("d = %.2f\n", d);
+        // }
         double minDistStrip = findMinDistStrip(points+start, end-start+1, d);
         if(minDistStrip < d){
             return minDistStrip;
@@ -119,13 +134,20 @@ double findMinDist(point2D *points, int len){
 double findMinDistStrip(point2D *points, int len, double minDist){
 
     double outputDist = minDist;
+
+    // copy the content of the array so that we don't mess up the orginal order after qsort
+    point2D copy[len];
+    for(int i = 0 ; i<len ; i++){
+        copy[i].x = points[i].x;
+        copy[i].y = points[i].y;
+    }
     
     // sort according to the y coordinate
-    qsort(points, len, sizeof(point2D), comparePointsY);
+    qsort(copy, len, sizeof(point2D), comparePointsY);
 
     for(int i = 0 ; i<len ; i++){
-        for(int j = i+1 ; j<len && (points[j].y-points[i].y)<minDist ; j++){
-            double dist = sqrt(pow(points[i].x-points[j].x, 2) + pow(points[i].y-points[j].y, 2));
+        for(int j = i+1 ; j<len && (copy[j].y-copy[i].y)<minDist ; j++){
+            double dist = sqrt(pow(copy[i].x - copy[j].x, 2) + pow(copy[i].y - copy[j].y, 2));
             if(dist < outputDist){
                 outputDist = dist;
             }
@@ -155,6 +177,6 @@ void findMinDistOSqr(point2D *points, int len){
         }
     }
     printf("The smallest distance is %.2f\n", minDist);
-    printf("x = %.1f, y = %.1f\n", pointA.x, pointA.y);
-    printf("x = %.1f, y = %.1f\n", pointB.x, pointB.y);
+    printf("x = %.1f, y = %.3f\n", pointA.x, pointA.y);
+    printf("x = %.1f, y = %.3f\n", pointB.x, pointB.y);
 }
